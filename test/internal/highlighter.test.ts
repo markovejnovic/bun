179fe7fe634beac21cc581c0bb82beb133234a81
Marko Vejnovic <marko.vejnovic@hotmail.com>
2025-07-18 19:28:18 -0700

    (17327): Fix interpolated string highlighting

--- test/internal/highlighter.test.ts
+++ test/internal/highlighter.test.ts
@@ -1,7 +1,20 @@
 import { highlightJavaScript as highlighter } from "bun:internal-for-testing";
 import { expect, test } from "bun:test";
+import stripAnsiColors from "strip-ansi";
 
 test("highlighter", () => {
   expect(highlighter("`can do ${123} ${'123'} ${`123`}`").length).toBeLessThan(150);
   expect(highlighter("`can do ${123} ${'123'} ${`123`}`123").length).toBeLessThan(150);
+});
+
+test("highlighter-with-multiline-interpolated-string", () => {
+  const testStr = '`foo: \${bar';
+  const highlighted = highlighter(testStr);
+  expect(stripAnsiColors(highlighted)).toBe(testStr);
+});
+
+test("highlighter-with-interpolated-string", () => {
+  const testStr = '`foo: \${bar}';
+  const highlighted = highlighter(testStr);
+  expect(stripAnsiColors(highlighted)).toBe(testStr);
 });
